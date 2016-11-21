ad56b45fb03f2b1b8e5e4ab89630c1b8b1e43635
Tadas Majeris <tadas.majeris@gmail.com>
2016-11-21 23:20:00 +0000

    security level 2000

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -2,6 +2,12 @@ import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
 import './task.html';
 
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  },
+});
+
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
@@ -9,5 +15,8 @@ Template.task.events({
   },
   'click .delete'() {
     Meteor.call('tasks.remove', this._id);
+  },
+  'click .toggle-private'() {
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
   },
 });
